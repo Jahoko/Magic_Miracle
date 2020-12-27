4b050637b3f4f5914e4e593e95c53da4d5790439
Joan Miro <73440422+Jahoko@users.noreply.github.com>
2020-12-27 20:21:24 +0100

    Second Commit

---
+++ Message.js
@@ -0,0 +1,21 @@
+// File with scripted Messages
+
+const moment = require("moment");
+
+function createMessage(){
+
+    // Erstelle ein zugällig generiertes Datum innerhalb des nächsten Jahres an dem Ereignis auftritt 
+    const dateStart=new Date();  
+    const endDate=new Date(2021,11,31,23,59,59)
+    let date = new Date(+dateStart + Math.random()*(endDate - dateStart));
+    //console.log(dateStart.toString(), endDate.toString(), date.toString())
+
+    fortuneArray=['Geld', 'Glück', 'Gesundheit', 'Weisheit', 'Liebe', 'Freude', 'ein Lächeln', 'einer Chance']
+
+    console.log('Am ' + new Intl.DateTimeFormat('de').format(date) + ' wird das Leben Dir ' + fortuneArray[Math.floor(Math.random()*8)] + ' schenken!')
+    
+
+    
+}
+createMessage();
+
